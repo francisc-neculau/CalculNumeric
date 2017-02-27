d9766b37f7daff471dec4acd36ef36870cdb5eaa
francisc-neculau <francisc.neculau@gmail.com>
2017-02-27 14:22:57 +0200

    Initial Commit

---
+++ CalculNumeric/src/GenericNumber.java
@@ -0,0 +1,24 @@
+
+public abstract class GenericNumber<T extends Number>
+{
+	private T number;
+
+	public GenericNumber(T number)
+	{
+		this.number = number;
+	}
+
+	public abstract GenericNumber<T> add(GenericNumber<T> otherNumber);
+	public abstract GenericNumber<T> subtract(GenericNumber<T> otherNumber);
+	public abstract GenericNumber<T> multiply(GenericNumber<T> otherNumber);
+	public abstract GenericNumber<T> divide(GenericNumber<T> otherNumber);
+	
+	public abstract boolean compareTo(GenericNumber<T> anotherNumber);
+	
+	@Override
+	public String toString()
+	{
+		return number.toString();
+	}
+
+}
---
+++ CalculNumeric/src/Main.java
@@ -0,0 +1,85 @@
+public class Main
+{
+
+	public static void main(String[] args)
+	{
+		Tema1<DoubleNumber> tema = new Tema1<DoubleNumber>();
+		//tema.start2();
+	}
+
+}
+
+class Tema1<T extends GenericNumber<?>>
+{
+	public void start()
+	{
+		Double machinePrecisionNumber;
+		Double denominator, numerator; // numerator/denominator
+		denominator = 1.0;
+		numerator = 1.0;
+		Double step = 10.0;
+		
+		Double precisionUnit = 1.0;
+		
+		do
+		{
+			denominator = step * denominator;
+			machinePrecisionNumber = numerator / denominator;
+			if((precisionUnit + machinePrecisionNumber) == precisionUnit) break;
+		} while(true);
+
+		System.out.println(machinePrecisionNumber);
+		
+		/* ********************************** */
+		
+		Double x, y, z;
+		x = 1.0;
+		y = machinePrecisionNumber;
+		z = machinePrecisionNumber;
+		
+		Double firstSum, secondSum;
+		firstSum = (( x + y ) + z);
+		secondSum = ( x + ( y + z ));
+
+		System.out.println("( x + y ) + z = " + firstSum);
+		System.out.println("x + ( y + z ) = " + secondSum);
+		if(firstSum.compareTo(secondSum) != 0)
+			System.out.println("neasociativa");
+		else 
+			System.out.println("asociativa");
+		
+		
+		Double firstProduct, secondProduct;
+		firstProduct = (( x * y ) * z);
+		secondProduct = ( x * ( y * z ));
+		
+		System.out.println("( x * y ) * z = " + firstProduct);
+		System.out.println("x * ( y * z ) = " + secondProduct);
+		if(firstProduct.compareTo(secondProduct) != 0)
+			System.out.println("neasociativa");
+		else 
+			System.out.println("asociativa");
+	}
+
+	public void start2(GenericNumber<Number> machinePrecisionNumber, GenericNumber<Number> precisionUnit, GenericNumber<Number> denominator, GenericNumber<Number> numerator, GenericNumber<Number> step)
+	{
+//		Double machinePrecisionNumber;
+//		Double denominator, numerator; // numerator/denominator
+//		denominator = 1.0;
+//		numerator = 1.0;
+//		Double step = 10.0;
+//		
+//		Double precisionUnit = 1.0;
+		
+		do
+		{
+			denominator = step.multiply(denominator);
+			machinePrecisionNumber = numerator.divide(denominator);
+			if((precisionUnit.add(machinePrecisionNumber)).compareTo(precisionUnit)) break;
+		} while(true);
+
+		System.out.println(machinePrecisionNumber);
+		
+		/* ********************************** */
+	}
+}
